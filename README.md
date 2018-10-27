6b5078a4e4b826207d40ee95af85942c9b328871
Roopak Venkatakrishnan <roopak.v@gmail.com>
2018-10-27 11:29:40 -0700

    Empty extension, doesn’t do much yet

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # spoke-chrome-extension
 [Unofficial] Chrome extension which works with askspoke.com
+
+To build bundle.
+```
+webpack --config webpack.js
+``` 
---
+++ webpack.js
@@ -0,0 +1,58 @@
+'use strict';
+
+console.log('sdds');
+const path = require('path');
+const webpack = require('webpack');
+
+console.log('sdds');
+
+const ConcatPlugin = require('webpack-concat-plugin');
+
+const vendorFiles = [
+  './node_modules/angular/angular.min.js',
+  './node_modules/angular-animate/angular-animate.min.js',
+  './node_modules/angular-aria/angular-aria.min.js',
+  './node_modules/angular-messages/angular-messages.min.js',
+  './node_modules/angular-material/angular-material.min.js',
+  './node_modules/angular-ui-router/release/angular-ui-router.min.js'
+];
+
+module.exports = {
+  devtool: 'source-map',
+  mode: 'development',
+  entry: {
+    'app': './lib/app/app.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist/')
+  },
+  resolve: {
+    extensions: ['.js', '.json']
+  },
+  plugins: [
+    new ConcatPlugin({
+      useHash: false,
+      sourceMap: true,
+      name: 'vendor',
+      fileName: '[name].js',
+      filesToConcat: vendorFiles
+    }),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+  module: {
+    rules: [{
+      test: /\.js$/,
+      include: [
+        path.resolve(__dirname, 'lib')
+      ],
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          babelrc: false,
+          presets: [['@babel/env', { loose: true, modules: false }]]
+        }
+      }]
+    }]
+  }
+};
